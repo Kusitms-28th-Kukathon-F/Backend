7b24d70bb63dc0ff9e2bade6618b00cbe861e3d7
hojeong2747 <hojeong2747@gmail.com>
2023-10-08 00:13:05 +0900

    fix: Swagger 403 에러 처리 * -> **

--- src/main/java/kusitms/server/domain/global/WebConfig.java
+++ src/main/java/kusitms/server/domain/global/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://kusitms.shop/swagger-ui/*")
+                .allowedOriginPatterns("https://kusitms.shop/swagger-ui/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true)
