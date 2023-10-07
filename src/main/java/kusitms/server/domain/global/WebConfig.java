0543e3da270e419e407af9ce85840ac00221ca57
hojeong2747 <hojeong2747@gmail.com>
2023-10-07 23:47:21 +0900

    fix: 프론트 Cors 에러 처리 (#17)

--- src/main/java/kusitms/server/domain/global/WebConfig.java
+++ src/main/java/kusitms/server/domain/global/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+//                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 //                .allowCredentials(true)
                 .allowedHeaders("*")
