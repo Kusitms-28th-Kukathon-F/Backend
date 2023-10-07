5272434aae71867a637ede3c4aad3333636b7823
OJOJIN <ojy101039@gmail.com>
2023-10-08 00:48:51 +0900

    fix: 지난 달별 조회 API 추가 (#16)

---
+++ src/main/java/kusitms/server/domain/tumbler/history/controller/TumblerHistoryController.java
@@ -0,0 +1,27 @@
+package kusitms.server.domain.tumbler.history.controller;
+
+
+import kusitms.server.domain.common.dto.SuccessResponse;
+import kusitms.server.domain.common.dto.code.SuccessCode;
+import kusitms.server.domain.tumbler.history.dto.response.HistoryMonthDetailResponseDto;
+import kusitms.server.domain.tumbler.history.service.TumblerHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/tumblers/history")
+public class TumblerHistoryController {
+
+    private final TumblerHistoryService tumblerHistoryService;
+
+    // ex. 202310 이정도로 달까지만 주세요
+    @GetMapping
+    public ResponseEntity<SuccessResponse<List<HistoryMonthDetailResponseDto>>> findDetailByMonth(@RequestParam String period, @RequestParam Long userId) {
+        List<HistoryMonthDetailResponseDto> request = tumblerHistoryService.findDetailByMonth(period, userId);
+        return SuccessResponse.of(SuccessCode.OK, request);
+    }
+}
---
+++ src/main/java/kusitms/server/domain/tumbler/history/service/TumblerHistoryService.java
@@ -0,0 +1,84 @@
+package kusitms.server.domain.tumbler.history.service;
+
+import kusitms.server.domain.common.error.ApplicationError;
+import kusitms.server.domain.common.error.NotFoundException;
+import kusitms.server.domain.company.entity.Company;
+import kusitms.server.domain.company.repository.CompanyRepository;
+import kusitms.server.domain.department.entity.Department;
+import kusitms.server.domain.department.repository.DepartmentRepository;
+import kusitms.server.domain.tumbler.history.dto.response.HistoryMonthDetailResponseDto;
+import kusitms.server.domain.tumbler.history.entity.TumblerHistory;
+import kusitms.server.domain.tumbler.history.repository.TumblerHistoryRepository;
+import kusitms.server.domain.user.entity.User;
+import kusitms.server.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static kusitms.server.domain.common.error.ApplicationError.DEPARTMENT_NOT_FOUND;
+import static kusitms.server.domain.common.error.ApplicationError.USER_NOT_FOUND;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class TumblerHistoryService {
+
+    private final TumblerHistoryRepository tumblerHistoryRepository;
+    private final DepartmentRepository departmentRepository;
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
+
+
+    public List<HistoryMonthDetailResponseDto> findDetailByMonth(String period, Long userId) {
+        User finduser = getUserById(userId);
+        Department userDepartment = getDepartmentByUser(finduser);
+        Company company = userDepartment.getCompany();
+        List<Department> departments = departmentRepository.findAllByCompany(company);
+        List<HistoryMonthDetailResponseDto> response = new ArrayList<>();
+        LocalDateTime startMonthDate = createStartMonthDate(period);
+        LocalDateTime endMonthDate = createEndMonthDate(period);
+        departments.forEach(department ->
+                response.add(
+                        HistoryMonthDetailResponseDto.of(findHistoryInPeriod(startMonthDate, endMonthDate, department))
+                )
+        );
+        return response;
+    }
+
+
+    // 월별 검색을 할때 1일 0시0분0초 값을 추가 해줘서 범위의 처음 값을 만듭니다.
+    private LocalDateTime createStartMonthDate(String period) {
+        period += "01";
+        LocalDateTime startDateTime = LocalDate.parse(period, DateTimeFormatter.ofPattern("yyyyMMdd")).atTime(0,0,0);
+        return startDateTime;
+    }
+
+    // 월별 검색을 할때 31일 23시59분59초 값을 추가 해줘서 범위의 마지막 값을 만듭니다.
+    private LocalDateTime createEndMonthDate(String period) {
+        period += "31";
+        LocalDateTime endDateTime = LocalDate.parse(period, DateTimeFormatter.ofPattern("yyyyMMdd")).atTime(23,59,59);
+        return endDateTime;
+    }
+
+    private User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
+    }
+
+    private Department getDepartmentByUser(User user) {
+        return departmentRepository.findByUser(user)
+                .orElseThrow(() -> new NotFoundException(DEPARTMENT_NOT_FOUND));
+    }
+
+    private TumblerHistory findHistoryInPeriod(LocalDateTime searchStartDate, LocalDateTime searchEndDate, Department department) {
+        TumblerHistory tumblerHistory = tumblerHistoryRepository.findByCreatedAtBetweenAndDepartmentOrderByTumblerPercentDesc(searchStartDate, searchEndDate, department);
+        return tumblerHistory;
+    }
+
+}
